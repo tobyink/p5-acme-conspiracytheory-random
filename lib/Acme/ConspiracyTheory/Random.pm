1c320689a258b699eefec232ba0e1b60ffcdb97b
Toby Inkster <mail@tobyinkster.co.uk>
2021-02-02 23:07:14 +0000

    bump version; changelog

--- lib/Acme/ConspiracyTheory/Random.pm
+++ lib/Acme/ConspiracyTheory/Random.pm
@@ -5,7 +5,7 @@ use warnings;
 package Acme::ConspiracyTheory::Random;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.008';
+our $VERSION   = '0.009';
 
 use Exporter::Shiny qw( theory );
 use List::Util 1.54 ();
