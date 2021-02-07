85fcf6024171f041d479e73541be2ff2cf42b0cb
Toby Inkster <mail@tobyinkster.co.uk>
2021-02-07 08:50:09 +0000

    bump version, changelog

--- lib/Acme/ConspiracyTheory/Random.pm
+++ lib/Acme/ConspiracyTheory/Random.pm
@@ -7,7 +7,7 @@ use warnings;
 package Acme::ConspiracyTheory::Random;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.012';
+our $VERSION   = '0.013';
 
 use Exporter::Shiny qw( theory bad_punctuation );
 use List::Util 1.54 ();
